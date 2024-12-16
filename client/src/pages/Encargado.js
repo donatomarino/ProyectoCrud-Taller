156f0529641f2ad1326c625f3197e6b733aaf596
aldaydev <rafaldayparejo@gmail.com>
2024-12-16 19:49:18 +0100

    Adding git ignore

---
+++ client/src/pages/Encargado.js
@@ -0,0 +1,9 @@
+const Encargado = ()=>{
+    return(
+        <main>
+            SOY LA PAGINA DE ENCARGADO
+        </main>
+    )
+}
+
+export default Encargado;
---
+++ client/src/pages/Login.js
@@ -0,0 +1,9 @@
+const Login = ()=>{
+    return(
+        <form>
+            SOY EL LOGIN
+        </form>
+    )
+}
+
+export default Login;
