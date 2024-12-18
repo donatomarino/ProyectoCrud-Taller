7d7c1471a9ac8c594cdf651c3df1f2f5d80ae88f
aldaydev <rafaldayparejo@gmail.com>
2024-12-18 19:38:43 +0100

    Formulario para añadir indicencias

---
+++ client/src/components/FormAddSolicitud.js
@@ -0,0 +1,51 @@
+const FormAddSolicitud = ()=>{
+
+    return (
+        <div>
+
+            <form onSubmit={handleSubmit} className="addPieza-form">
+                <h3 className="addPieza-title">Añadir Nueva Pieza</h3>
+                <input
+                name="id"
+                placeholder="ID"
+                // value={nuevaPieza.id}
+               
+                className="addPieza-textInput"
+                />
+                <input
+                name="marca"
+                placeholder="Marca"
+                // value={nuevaPieza.nombre}
+             
+                className="addPieza-textInput"
+                />
+
+                <input
+                name="tipo"
+                placeholder="Tipo"
+                // value={nuevaPieza.tipo}
+             
+                className="addPieza-textInput"
+                />
+                
+                <input
+                name="precio_compra"
+                placeholder="Precio Compra"
+                // value={nuevaPieza.precio_compra}
+            
+                className="addPieza-textInput"
+                />
+                <input
+                name="precio_venta"
+                placeholder="Precio Venta"
+                // value={nuevaPieza.precio_venta}
+               
+                className="addPieza-textInput"
+                />
+                <button type="submit" className="addPieza-submitBtn">AÑADIR</button>
+            </form>
+        </div>
+    )
+}
+
+export default FormAddSolicitud;
