237a6f526bf63023675756e218be21e27c46b92d
leodev_matebook <jaime.leo.caicedo@gmail.com>
2024-12-18 11:38:00 -0500

    Nuevos endpoints creados /create-query y /list-queries

--- server/controller.js
+++ server/controller.js
@@ -1,7 +1,6 @@
+// Jaime / 18-12-2024 / Funciones del controller con respuesta del servidor / 1.0.1
 //----Importación authOperations
-import { auth } from './authOperations.js';
-
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signIn } from './authOperations.js';
 
 //----Importación MongoOperations
 import {
@@ -20,8 +19,8 @@ export const login = async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        // en credentials guarda el resultado de la función signInWithEmailAndPassword
-        const credentials = await signInWithEmailAndPassword(auth, email, password);
+        // en credentials guarda el resultado de la función signIn
+        const credentials = await signIn(email, password);
 
         console.log('CONECTADO CORRECTAMENTE');
 
@@ -105,5 +104,61 @@ export const createTool = async (req, res) => {
     } catch (error) {
         console.error("Error al crear herramienta:", error);
         res.status(500).json({ message: "Error al crear herramienta", error });
+    }
+};
+
+// Crear una nueva solicitud
+export const createRequest = async (req, res) => {
+    try {
+        // los datos de la solicitud se almacenan en esta constante
+        const newRequest = {
+            tipo: req.body.tipo,
+            titulo: req.body.titulo,
+            descripcion: req.body.descripcion,
+            resuelta: "false",
+        };
+
+        // Insertar el documento en la base de datos
+        await insertarDocumento("solicitudes", newRequest);
+
+        console.log("--- Nueva solicitud creada ----");
+
+        // Responder con status 200 y un mensaje
+        res.status(200).json({ message: "Solicitud creada exitosamente" });
+    } catch (error) {
+        console.error("Error al crear la solicitud:", error);
+        res.status(500).json({ message: "Error al crear la solicitud", error });
+    }
+};
+
+// Mostrar todas las solicitudes
+export const allRequests = async (req, res) => {
+    try {
+        // Obtener todos los elementos desde la base de datos
+        const requests = await verTodos("solicitudes");
+
+        console.log("--- Todos las solicitudes obtenidos correctamente ---");
+
+        // Verificar si la coleccion "solicitudes" está vacío
+        if (!requests || requests.length === 0) {
+            console.warn("No hay datos que recuperar de la base de datos.");
+            return res.status(401).json({
+                message: "No hay datos que recuperar de la base de datos",
+            });
+        }
+
+        // Responder con estado 200 y los datos obtenidos
+        return res.status(200).json({
+            message: "solicitudes obtenidas exitosamente",
+            data: requests
+        });
+    } catch (error) {
+        console.error("Error al obtener las solicitudes:", error);
+
+        // Manejo de errores generales
+        return res.status(500).json({
+            message: "Error al obtener las solicitudes",
+            error: error.message
+        });
     }
 };
--- server/router.js
+++ server/router.js
@@ -1,11 +1,14 @@
+// Jaime / 18-12-2024 / Rutas del EndPoint del servidor / 1.0.1
 import { Router } from "express";
-import { index, login, allItems, createTool } from "./controller.js";
+import { index, login, allItems, createTool, createRequest, allRequests } from "./controller.js";
 
 const router = Router();
 
 router.get("/", index);
-router.post("/login", login)
-router.get("/all-items", allItems)
-router.post("/create-tool", createTool)
+router.post("/login", login);
+router.get("/all-items", allItems);
+router.post("/create-tool", createTool);
+router.post("/create-query", createRequest);
+router.get("/list-queries", allRequests);
 
 export default router
