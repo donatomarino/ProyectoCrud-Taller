104e2cbd6a07c59f2f0309077d4a320d3650308d
donatomarino <donato_8@icloud.com>
2024-12-19 22:28:10 +0100

    Arreglo fichero Swagger

--- server/app.js
+++ server/app.js
@@ -6,10 +6,6 @@ import { swaggerDocs, swaggerUi } from "./swaggerConfig.js";
 //----Importación de router
 import router from './router.js';
 
-//----Importación Swagger
-import swaggerUi from "swagger-ui-express";
-import { swaggerDocs } from "./swaggerOptions.js"
-
 const FRONTEND_URL = process.env.FRONTEND_URL || "http://127.0.0.1:3000";
 const app = express()
 app.use(
@@ -24,9 +20,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // Carlos / 19-12-2024 / swagger / 1.0.0
 // Ruta para la documentación de Swagger
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-// Configuración de Swagger UI
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use(router)
